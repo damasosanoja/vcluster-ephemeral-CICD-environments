1aa26065732f7fce23edf92cbdd726b2b6b014bb
damasosanoja <damasosanoja@gmail.com>
2023-09-16 16:56:16 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  backend "kubernetes" {
+    secret_suffix = "common"
+    config_path   = "~/.kube/config"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "vcluster-v1.27.5-k3s1" {
+  count = 3
+  namespace = "vcluster-k3s1275--${count.index}"
+  name      = "vcluster-k3s1275-${count.index}"
+  repository       = "https://charts.loft.sh"
+  chart            = "vcluster"
+  create_namespace = true
+  values = [file("${path.module}/vcluster-v1.27.5-k3s1.yaml")]
+}
